634c375dbed815b22f48d43bd322e7c22bb0d630
Chandrashekar Arun Naik <canaik24@gmail.com>
2020-04-05 16:53:48 +0530

    Add files via upload

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Goldy.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        public IActionResult Large()
+        {
+            return View();
+
+        }
+        public IActionResult Small()
+        {
+            return View();
+
+        }
+        public IActionResult Analysis()
+        {
+            return View();
+
+        }
+        public IActionResult Bangalore()
+        {
+            return View();
+
+        }
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+   
+}
+
